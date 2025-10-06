63de71da0f998969a2267ba41ccb7a120e7f1277
07mercedes-source <07mercedes@gmail.com>
2025-10-06 15:51:16 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,11 @@
-import '../styles-global.css';
+import '../styles/globals.css';
 import Navbar from '../components/Navbar';
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-  const [userName, setUserName] = useState('');
-
-  useEffect(() => {
-    const storedName = localStorage.getItem('userName');
-    if (storedName) setUserName(storedName);
-    else if (router.pathname !== '/login') router.push('/login');
-  }, [router]);
-
   return (
     <>
-      <Navbar userName={userName} setUserName={setUserName} />
-      <Component {...pageProps} setUserName={setUserName} />
+      <Navbar userName="Ali Veli" onLanguageChange={(lang)=>console.log(lang)} />
+      <Component {...pageProps} />
     </>
   );
 }
