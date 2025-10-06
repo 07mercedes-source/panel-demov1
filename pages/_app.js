56e6a7b2bc92f5b02cb03c7f37c0b3afd5c52691
07mercedes-source <07mercedes@gmail.com>
2025-10-06 14:57:57 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
 import '../styles-global.css';
+import Navbar from '../components/Navbar';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const [userName, setUserName] = useState('');
+
+  // localStorage'dan kullanıcı adını al
+  useEffect(() => {
+    const storedName = localStorage.getItem('userName');
+    if (storedName) setUserName(storedName);
+    else if (router.pathname !== '/login') router.push('/login'); // login değilse ve kullanıcı yoksa yönlendir
+  }, [router]);
+
+  return (
+    <>
+      <Navbar userName={userName} />
+      <Component {...pageProps} setUserName={setUserName} />
+    </>
+  );
 }
